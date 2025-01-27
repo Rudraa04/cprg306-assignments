81ea2e22e98f0d90e387280692cde61097638b37
Rudraa04 <167252539+Rudraa04@users.noreply.github.com>
2025-01-27 11:58:49 -0700

    added student-info file

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Rudra Chiragkumar Patel</h1>
+      <p>      
+        <Link href="https://github.com/Rudraa04/cprg306-assignments">My Github Link:- https://github.com/Rudraa04/cprg306-assignments</Link>
+      </p>
+     
+    </div>
+  );
+}
